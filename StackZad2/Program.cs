212808c22e85dd8d4cf706e5eebe76eec274e486
MEGA <MEGA@gmail.com>
2025-03-17 13:18:31 +0200

    Add project files.

---
+++ StackZad2/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace StackZad2
+{
+    internal class Program
+    {
+        static bool CheckBalanced(string input)
+        {
+            Stack<char> stack = new Stack<char>(); 
+            var matchingBrackets = new Dictionary<char, char> 
+        {
+            { ')', '(' },
+            { '}', '{' },
+            { ']', '[' }
+        };
+
+            foreach (char ch in input)
+            {
+                if (matchingBrackets.ContainsValue(ch)) 
+                {
+                    stack.Push(ch);
+                }
+                else if (matchingBrackets.ContainsKey(ch)) 
+                {
+                    if (stack.Count > 0 && stack.Peek() == matchingBrackets[ch])
+                    {
+                        stack.Pop(); 
+                    }
+                    else
+                    {
+                        return false; 
+                    }
+                }
+            }
+
+            return stack.Count == 0; 
+        }
+        static void Main()
+        {
+            string input1 = "{[()()]}";
+            string input2 = "{[(])}";
+
+            Console.WriteLine($"Input: {input1} - Balanced: {CheckBalanced(input1)}");
+            Console.WriteLine($"Input: {input2} - Balanced: {CheckBalanced(input2)}");
+
+        }
+    }
+}
+
